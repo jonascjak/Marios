f905c34fbd84330a04ef547ac5586fec38668e90
Nunsen <nuso0001@stud.kea.dk>
2024-04-08 09:08:18 +0200

    Jeg har tilføjet en hovedmenu og en menu, samt metode til at læse og printe menu.txt i konsollen.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,81 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.time.LocalTime;
 
 public class Main {
-
     public static Scanner scan = new Scanner(System.in);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         mainMenu();
     }
 
-    public static void mainMenu(){
+    public static void mainMenu() throws FileNotFoundException {
+        int answer;
+
+        do {
+            System.out.println("**Velkommen til Mario's Pizzabar**\nVælg en af de følgende muligheder for at fortsætte");
+            System.out.println("Tast 1 for at Oprette Bestilling");
+            System.out.println("Tast 2 for at Vise Menukort");
+            System.out.println("Tast 3 for at Vise Statistik");
+            System.out.println("Tast 0 for at afslutte Hovedmenu");
+            answer = scan.nextInt();
+
+            switch (answer) {
+                case 1:
+                    System.out.println("Opret ny bestilling");
+                    addOrder();
+
+                    break;
+
+
+                case 2:
+                    System.out.println("Her er Mario's menukort:");
+                    showMenu();
+
+                    break;
+                case 3:
+                    System.out.println("Oversigt over omsætning samt Top 5 Pizzaer:");
+                    //showStatistics();
+
+                    break;
+
+                case 0:
+                    System.out.println("Tak for denne gang.");
+
+                default:
+                    System.out.println("Ugyldig valgmulighed, vælg venligst enten 1, 2 eller 3.");
+            }
+        } while (answer != 0);
+
+    }
+
+    public static String showMenu() throws FileNotFoundException {
+        Scanner menuFile = new Scanner(new File("C:\\Users\\PC\\IdeaProjects\\projekt test\\src\\menu"));
+
+        while (menuFile.hasNextLine()) {
+            System.out.println(menuFile.nextLine());
+
+        }
+        menuFile.close();
+        return menuFile.toString();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void addOrder(){
         ArrayList<Pizza> Pizzas = new ArrayList<>();
         Pizza first = new Pizza(scan.nextInt(),scan.nextLine());
         Pizza second= new Pizza(scan.nextInt(),scan.nextLine());
