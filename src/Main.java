7893bfa395c2b83a48f42b98efe4cfde5c5942ae
Nunsen <nuso0001@stud.kea.dk>
2024-04-09 14:07:54 +0200

    CLEAN SLATE

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Collections.*;
 import java.util.Scanner;
 import java.time.LocalTime;
 import static java.time.LocalTime.*;
@@ -12,6 +9,7 @@ public class Main {
     public static Scanner scan = new Scanner(System.in);
     public static ArrayList<Order> orderList = new ArrayList();
     public static LocalTime time = now();
+
     public static void main(String[] args) throws FileNotFoundException {
         mainMenu();
     }
@@ -19,10 +17,11 @@ public class Main {
     public static void showList() {
         System.out.println("Bestillingsliste:");
 
-        for (Order orders : orderList) {
-            System.out.println("Order Time: " + orderList.getOrderTime());
+        for (Order Orders : orderList) {
+            System.out.println(Orders);
         }
     }
+
     public static void mainMenu() throws FileNotFoundException {
         int answer;
 
@@ -55,20 +54,17 @@ public class Main {
                 case 4:
                     System.out.println("Her er Mario's menukort:");
                     showMenu();
-
                     break;
+
                 case 5:
                     PrintRevenueAndTopFive();
                     break;
 
-                default:
-                    System.out.println("Ugyldig valgmulighed, vælg venligst enten 1, 2 eller 3.");
             }
         } while (answer != 0);
         System.out.println("Tak for denne gang.");
         System.exit(0);
     }
-
     public static void showMenu() throws FileNotFoundException {
         Scanner menuFile = new Scanner(new File("src/menu.txt"));
         while (menuFile.hasNextLine()) {
