718f39157d3b93b96996a5a8df0747c3c4496186
Nunsen <nuso0001@stud.kea.dk>
2024-04-10 11:03:47 +0200

    Tilføjet den nye sorterede arraylist, som nu sorterer bestillingslisten efter tid. Dertil har jeg lavet små stavefejlsrettelser og omformuleret nogle dele. Tilføjet nogle kommentarer.

--- src/MariosPizzabar.java
+++ src/MariosPizzabar.java
@@ -1,11 +1,15 @@
+//Første tværfaglige projekt - April 2024, (Fatima, Jonas, Mads & Nunu)
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 import java.time.LocalTime;
 import static java.time.LocalTime.*;
 
-public class Main {
+public class MariosPizzabar {
+    //public static så det er tilgængeligt for alle, uden at parameteroverføre
     public static Scanner scan = new Scanner(System.in);
     public static ArrayList<Order> orderList = new ArrayList();
     public static ArrayList<Pizza> allPizzaList = new ArrayList();
@@ -19,11 +23,19 @@ public class Main {
     public static void showList() {
         System.out.println("Bestillingsliste:");
 
-        for (Order Orders : orderList) {
-            System.out.println(Orders);
+        /*Da collections.sort kun kan sortere efter numerisk eller alfabetisk rækkefølge bruger vi
+        comparator for at sortere efter LocalTime orderTime*/
+        orderList.sort(Comparator.comparing(t->t.orderTime));
+
+        for (Order Orders:orderList) {
+            System.out.println(Orders.orderNumber);
+            System.out.print(Orders.orderTime + " ");
+            System.out.print(Orders.order + " ");
+            System.out.print("Leveringsadresse:" + Orders.deliveryAddress + " ");
+            System.out.println();
+            System.out.println();
         }
     }
-
     public static void mainMenu() throws FileNotFoundException {
         int answer;
 
@@ -69,6 +81,8 @@ public class Main {
     }
     public static void showMenu() throws FileNotFoundException {
         Scanner menuFile = new Scanner(new File("src/menu.txt"));
+
+        //printer menu.txt filen så længe der er en ny linje at læse
         while (menuFile.hasNextLine()) {
             System.out.println(menuFile.nextLine());
         }
@@ -79,36 +93,41 @@ public class Main {
         ArrayList<Pizza> Pizzas = new ArrayList<>();
         int answer;
         double totalPrice = 0;
+
         do {
-            System.out.println("Hvad er nummeret på pizzaen som er blevet bestilt?");
+            System.out.println("Indtast pizzanummer: ");
             try {
                 Pizza first = allPizzaList.get(scan.nextInt() - 1);
                 Pizzas.add(first);
                 totalPrice += first.pizzaPrice;
             } catch(Exception e){
-                System.out.println("Den pizza findes ikke i systemet");
+                System.out.println("Den pizza findes ikke i systemet.");
             }
             System.out.println("Vil du tilføje flere pizza'er?\nTast 1 for ja\nTast 2 for nej");
             answer = scan.nextInt();
+
         } while (answer != 2);
 
         String deliveryAddress = null;
         LocalTime deliveryTime = null;
+
+        //Bruger LocalTime til at vælge bestemt bestillings tidspunkt ->bruges til at sortere array
         LocalTime time = now();
-        System.out.println("Skal bestillingen leveres på andet specielt tidspunkt?\nTast 1 for ja\nTast 2 for nej");
+
+        System.out.println("Skal bestillingen afhentes på et bestemt tidspunkt?\nTast 1 for ja\nTast 2 for nej");
         int whatTime = scan.nextInt();
 
         if (whatTime != 2) {
-            System.out.println("Indtast tidspunkt");
+            System.out.println("Indtast tidspunkt:");
             deliveryTime = LocalTime.parse(scan.next());
         }else {
             deliveryTime = time.plusMinutes(15);
         }
-        System.out.println("Skal bestillingen levers?\nTast 1 for ja\nTast 2 for nej");
+        System.out.println("Skal bestillingen leveres?\nTast 1 for ja\nTast 2 for nej");
         int beDelivered = scan.nextInt();
 
         if (beDelivered != 2) {
-            System.out.println("Hvad er leverings addressen?");
+            System.out.println("Indtast leveringsaddressen:");
             deliveryAddress = scan.next();
             Order currentOrder = new Order(orderList.size() + 1, deliveryAddress, deliveryTime.withNano(0), totalPrice, Pizzas);
             orderList.add((currentOrder));
